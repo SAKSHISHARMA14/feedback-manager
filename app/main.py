d2e7c52e2ddd028a982ef126791b48a1387be75f
Sakshi Sharma <Sakshi Sharma@inindcrws.com>
2026-01-16 07:22:45 +0530

    Ignore W391 flake8 rule

--- app/main.py
+++ app/main.py
@@ -39,4 +39,4 @@ def submit():
     return redirect(url_for("index"))
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000, debug=True)
+    app.run(host="127.0.0.1", port=5000, debug=True)    #app.run(host="0.0.0.0", port=5000, debug=True)
